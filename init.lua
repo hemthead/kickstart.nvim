8a9f859be950bbaee8a3ac07987ba59eac5643bf
John Reed <reallyjohnreed@gmail.com>
2025-08-31 23:09:18 -0400

    I'm temporarily tired of tabs in vimwiki

--- init.lua
+++ init.lua
@@ -168,6 +168,9 @@ vim.o.scrolloff = 10
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
+-- I'm getting real tired of vim's bullcrap, here
+vim.o.expandtab = true
+
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
